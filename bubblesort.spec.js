9ef36294ccff7faa0a0c50e3985958201c56315e
AlisaBelyakova <6183112@gmail.com>
2018-07-25 17:19:00 -0700

    bubblesort tests

---
+++ bubblesort.spec.js
@@ -0,0 +1,30 @@
+describe('Bubble Sort', function () {
+    it('handles an empty array', function () {
+        expect(bubbleSort([])).toEqual([]);
+    });
+
+    it('handles an array with one element', function () {
+        expect(bubbleSort([0])).toEqual([0]);
+        expect(bubbleSort([5])).toEqual([5]);
+
+        expect(bubbleSort([13]).length).toEqual(1);
+        expect(bubbleSort(['apachi']).length).toEqual(1);
+
+        expect(bubbleSort(['ant'])).toEqual(['ant']);
+        expect(bubbleSort(['banana'])).toEqual(['banana']);
+
+    });
+
+    it('handles an array with multiple elements', function () {
+        expect(bubbleSort([34, 90])).toEqual([34, 90]);
+        expect(bubbleSort([6, 0, 18, 4, 31])).toEqual([0, 4, 6, 18, 31]);
+        expect(bubbleSort([5, 1, 7, 22, 9, 3, 2, 19, 150, 4, 11])).toEqual([1, 2, 3, 4, 5, 7, 9, 11, 19, 22, 150]);
+
+        expect(bubbleSort([5, 7, 8, 1]).length).toEqual(4);
+        expect(bubbleSort(['a', 'b', 'c']).length).toEqual(3);
+
+        expect(bubbleSort(['z', 'y', 'x'])).toEqual(['x', 'y', 'z']);
+        expect(bubbleSort(['dog', 'banana', 'ant', 'apple', 'mango', 'cat'])).toEqual(['ant', 'apple', 'banana', 'cat', 'dog', 'mango']);
+
+    });
+});
